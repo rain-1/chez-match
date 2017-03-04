6c719465f540536052cee83dad4f9f944aae1f24
rain <rain1@openmailbox.org>
2017-03-04 16:13:21 +0000

    new match code

--- match.scm
+++ match.scm
@@ -1,162 +1,220 @@
-(library (match-definitions)
-(export trie-merge compile-pattern interpret-tree)
+(library (match-expanders)
+  (export push! push-box! pattern-expanders syntax-car syntax-cdr
+	  length=? length>=? append*
+	  join-syntax-symbols)
 (import (chezscheme))
 
 (define (push-box! b v)
   (set-box! b (cons v (unbox b))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define-syntax push!
+  (syntax-rules ()
+    ((push! place val) (set! place (cons val place)))))
 
-;; This is the code for merging a list of instructions into a trie
+(define pattern-expanders (box '()))
 
-(define terminator (cons '() '()))
+(define syntax-car
+  (lambda (ls)
+    (syntax-case ls ()
+      ((x . y) #'x))))
 
-(define (trie-merge seqs)
-  (cond ((null? seqs)
-	 '())
-	((member '() seqs)
-	 (cons terminator
-               (trie-merge (filter pair? seqs))))
-	(else (let* ((p (partition-by-head (caar seqs) seqs))
-		     (tails (car p))
-		     (rest (cdr p)))
-		(cons `(,(caar seqs) . ,(trie-merge tails))
-		      (trie-merge rest))))))
+(define syntax-cdr
+  (lambda (ls)
+    (syntax-case ls ()
+      ((x . y) #'y))))
 
-(define (partition-by-head head seqs)
-  (cond ((null? seqs)
-         terminator)
-	(else (let* ((p (partition-by-head head (cdr seqs)))
- 		     (l (car p))
- 		     (r (cdr p)))
-		(if (equal? (syntax->datum head)
-			    (syntax->datum (caar seqs)))
-		    (cons (cons (cdar seqs) l) r)
-		    (cons l (cons (car seqs) r)))))))
+(define (length=? l n)
+  ;; tests if a list has a certain length
+  ;; failing early if possible
+  ;; failing on non-lists
+  (let loop ((l l) (n n))
+    (cond ((< n 0) #f)
+	  ((null? l) (= n 0))
+	  ((pair? l) (loop (cdr l) (- n 1)))
+	  (else #f))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (length>=? l n)
+  (let loop ((l l) (n n))
+    (cond ((<= n 0) #t)
+	  ((null? l) #f)
+	  ((pair? l) (loop (cdr l) (- n 1)))
+	  (else #f))))
 
-;; This is the interpreter for trees of matching instructions.
-;; matching instructions are as defined in compile-pattern,
-;; and the trees terminate with an (execute <code>) command
+(define (append* n l t)
+  (if (= n 0)
+      (cons l t)
+      (cons (car l) (append* (- n 1) (cdr l) t))))
 
-;; The interpreter handles scope explicitly and manages a
-;; fail continuation to do backtracking.
+(define (join-syntax-symbols s1 . rest)
+  (datum->syntax s1 (string->symbol (apply string-append
+					   (symbol->string (syntax->datum s1))
+					   (map (lambda (s)
+						  (symbol->string (syntax->datum s)))
+						rest)))))
 
-(define-syntax interpret-tree
-  (syntax-rules (execute bind compare-equal? guard decons)
+)
 
-    ((interpret-tree scope () stack failure)
-     failure)
+(library (match-core)
+  (export match match-pats)
+(import (chezscheme) (match-expanders))
 
-    ((interpret-tree scope (((execute <body>) (())) <alternatives> ...) stack failure)
-     (let* scope <body>))
+(define-syntax match
+  (syntax-rules (else)
+    ((match <exp> (<pat> <body> ...) ...)
+     (let* ((t <exp>) (stack (list t)))
+       (match-aux t stack (<pat> (begin <body> ...)) ...)))
+    ((match <exp> (<pat> <body> ...) ... (else <body-2> ...))
+     (match <exp> (<pat> <body> ...) ... (_ <body-2> ...)))))
 
-    ((interpret-tree scope (((bind <var>) <then> ...) <alternatives> ...) stack failure)
-     (let ((top (car stack))
-           (new-stack (cdr stack)))
-       (interpret-tree ((<var> top) . scope) (<then> ...) new-stack
-		       (interpret-tree scope (<alternatives> ...) stack failure))))
+(define-syntax match-aux
+  (syntax-rules ()
+    ((match-aux t stack)
+     (error 'match "failed to match" t))
+    ((match-aux t stack (<pat-1> <body-1>) (<pat> <body>) ...)
+     (let ((fk (lambda () (match-aux t stack (<pat> <body>) ...))))
+       (match-pats (<pat-1>) stack <body-1> fk)))))
 
-    ((interpret-tree scope (((compare-equal? <s-expr>) <then> ...) <alternatives> ...)
-                     stack failure)
-     (let ((top (car stack))
-           (new-stack (cdr stack)))
-       (if (let* scope (equal? top <s-expr>))
-           (interpret-tree scope (<then> ...) new-stack failure)
-           (interpret-tree scope (<alternatives> ...) stack failure))))
-
-    ((interpret-tree scope (((guard <predicate>) <then> ...) <alternatives> ...)
-                     stack failure)
-     (if (let* scope <predicate>)
-         (interpret-tree scope (<then> ...) stack failure)
-         (interpret-tree scope (<alternatives> ...) stack failure)))
-    
-    ((interpret-tree scope (((decons) <then> ...) <alternatives> ...) stack failure)
-     (let ((top (car stack))
-           (new-stack (cdr stack))
-	   (fail-thunk (lambda ()
-			 (interpret-tree scope (<alternatives> ...) stack failure))))
-       (if (pair? top)
-           (let ((stack (cons (car top) (cons (cdr top) new-stack))))
-             (interpret-tree scope (<then> ...) stack (fail-thunk)))
-	   (fail-thunk))))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; This part compiles a pattern into a sequence of matching instructions
-;; that performs the testing and binding operations
-;;
-;; Update this part to change or extend the pattern language.
-
-;; The syntax of patterns used here is:
-;;
-;; <pat>  ::= <var> | (<atom> <pat> ...)
-;;
-;; these functions are used to turn a pattern into a sequence of matching instructions
-
-(define (var? s)
-  (symbol? (syntax->datum s)))
-(define (functor? s)
-  (let ((s (syntax->datum s)))
-    (and (pair? s) (symbol? (car s)))))
-
-(define (compile-pattern box pat rest)
-  (cond ((var? pat) (compile-var box pat rest))
-	((functor? pat)
-	 (let ((pat (syntax->list pat)))
-	   (compile-functor box (car pat) (cdr pat) rest)))
-	(else (error 'compile-pattern "Invalid pattern" pat))))
-
-(define (identifier-mem? x ls)
-  (and (not (null? ls))
-       (or (bound-identifier=? x (car ls))
-	   (identifier-mem? x (cdr ls)))))
-
-(define (compile-var box var rest)
-  (if (identifier-mem? var (unbox box))
-      (cons #`(compare-equal? #,var) (rest))
-      (begin
-	(push-box! box var)
-	(cons #`(bind #,var) (rest)))))
-
-(define (compile-functor box name args rest)
-  (list* #'(decons)
-	 #`(compare-equal? '#,name)
-	 (compile-args box args rest)))
-
-(define (compile-args box args rest)
-  (if (null? args)
-      (cons #'(compare-equal? '()) (rest))
-      (cons #'(decons)
-	    (compile-pattern box (car args)
-			     (lambda ()
-			       (compile-args box (cdr args)
-					     rest))))))
+(define-syntax match-pats
+  (lambda (stx)
+    (syntax-case stx ()
+      ((match-pats () stack sk fk)
+       #'(if (null? stack) sk (fk)))
+      ((match-pats (p . ps) stack sk fk)
+       (let ((pat^ (syntax->datum #'p)))
+	 (cond ((symbol? pat^)
+		#`(if (null? stack)
+		      (fk)
+		      (let ((p (car stack)) (stack (cdr stack)))
+			(match-pats ps stack sk fk))))
+	       ((or (null? pat^) (number? pat^)
+		    (boolean? pat^) (char? pat^))
+		#`(match-pats ('p . ps) stack sk fk))
+	       (else (let ((expander (assoc (car pat^) (unbox pattern-expanders))))
+		       (unless expander
+			 (error 'match-pats "no expander for pattern" ;(map car (unbox pattern-expanders))
+				pat^))
+		       ((cdr expander) (syntax->list #'p) #'ps #'stack #'sk #'fk)))))))))
 
 )
 
 (library (match)
-(export match)
-(import (chezscheme) (match-definitions))
+(export match pattern-expanders foo define-record-matcher)
+(import (rename (chezscheme) (define-record %define-record))
+	(match-expanders) (match-core))
 
-(define-syntax (match stx)
-  (define (compile-pattern^ entry)
-    (compile-pattern (box '())
-		     (car entry)
-		     (lambda ()
-		       (list #`(execute #,(cadr entry))))))
-  (syntax-case stx (else)
-    ((match <exp> (<pattern> <body> ...) ... (else <fail> ...))
-     (let* ((rules #'((<pattern> (begin <body> ...)) ...))
-	    (instructions (map compile-pattern^ (syntax->list rules)))
-	    (trie (trie-merge instructions)))
-       #`(let ((stack (list <exp>)))
-	   (interpret-tree ()
-			   #,trie
-			   stack
-			   (begin <fail> ...)))))
-    ((match <exp> (<pattern> <body> ...) ...)
-     #'(match <exp> (<pattern> <body> ...) ... (else (error 'match "Failed to match"))))))
+(define-syntax make-record-matcher
+  (syntax-rules ()
+    ((make-record-matcher rec rec? num-fields (get ...))
+     (push-box! pattern-expanders
+		(cons 'rec
+		      (lambda (p ps stack sk fk)
+			(let ((pps (cdr p)))
+			  (unless (= num-fields (length pps))
+			    (error 'make-record-matcher "bad record pattern" (syntax->datum p)))
+			  #`(if (null? #,stack)
+				(error 'quote-matcher "failure during record match")
+				(let ((top (car #,stack)) (stack (cdr #,stack)))
+				  (if (rec? top)
+				      (let ((stack (list* (get top) ... stack)))
+					(match-pats #,(append pps ps)
+						    stack #,sk #,fk))
+				      (#,fk)))))))))))
+
+(define-syntax define-record-matcher
+  (lambda (stx)
+    (syntax-case stx ()
+      ((define-record-matcher rec (field ...))
+       (let* ((getters (map (lambda (f)
+			      (join-syntax-symbols #'rec #'- f))
+			    (syntax->list #'(field ...))))
+	      (rec? (join-syntax-symbols #'rec #'?)))
+	 #`(make-record-matcher rec #,rec? #,(length getters) #,getters))))))
+
+;; > (import (match))
+;; > (define-record kons (kar kdr))
+;; > (define-record-matcher kons (kar kdr))
+;; > (match (make-kons 1 2) ((kons x y) (list y x)))
+;; (2 1)
+
+(define-syntax define-record
+  (syntax-rules ()
+    ((define-record <name> (<field> ...))
+     (begin (%define-record <name> (<field> ...))
+	    (define-record-matcher <name> (<field> ...))
+	    (record-reader '<name> (type-descriptor <name>))))))
+
+(define (foo) #t)
+
+(push-box! pattern-expanders
+	   (cons 'quote
+		 (lambda (p ps stack sk fk)
+		   #`(if (null? #,stack)
+			 (error 'quote-matcher "failure during match")
+			 (let ((top (car #,stack)) (stack (cdr #,stack)))
+			   (if (equal? '#,(cadr p) top)
+			       (match-pats #,ps
+					   stack #,sk #,fk)
+			       (#,fk)))))))
+
+(push-box! pattern-expanders
+	   (cons 'cons
+		 (lambda (p ps stack sk fk)
+		   (let ((p-1 (cadr p))
+			 (p-2 (caddr p)))
+		     #`(if (null? #,stack)
+			   (error 'quote-matcher "failure during match")
+			   (let ((top (car #,stack)) (stack (cdr #,stack)))
+			     (if (pair? top)
+				 (let ((stack (list* (car top) (cdr top) stack)))
+				   (match-pats #,(list* p-1 p-2 ps)
+					       stack #,sk #,fk))
+				 (#,fk))))))))
+
+(push-box! pattern-expanders
+	   (cons 'list
+		 (lambda (p ps stack sk fk)
+		   (let ((pps (cdr p)))
+		     #`(if (null? #,stack)
+			   (error 'quote-matcher "failure during match")
+			   (let ((top (car #,stack)) (stack (cdr #,stack)))
+			     (if (length=? top #,(length pps))
+				 (let ((stack (append top stack)))
+				   (match-pats #,(append pps ps)
+					       stack #,sk #,fk))
+				 (#,fk))))))))
+
+;; (append* 2 '(a b c d e f) '(foo))
+;; => (a b (c d e f) (foo))
+
+(push-box! pattern-expanders
+	   (cons 'list*
+		 (lambda (p ps stack sk fk)
+		   (let ((pps (cdr p)))
+		     #`(if (null? #,stack)
+			   (error 'quote-matcher "failure during match")
+			   (let ((top (car #,stack)) (stack (cdr #,stack)))
+			     (if (length>=? top #,(- (length pps) 1))
+				 (let ((stack (append* #,(- (length pps) 1) top stack)))
+				   (match-pats #,(append pps ps)
+					       stack #,sk #,fk))
+				 (#,fk))))))))
+
+(push-box! pattern-expanders
+	   (cons 'quasiquote
+		 (lambda (p ps stack sk fk)
+		   (let* ((q (cadr p)) (q^ (syntax->datum q)))
+		     (cond ((or (null? q^)
+				(symbol? q^) (number? q^)
+				(boolean? q^) (char? q^))
+			    #`(match-pats ('#,q . #,ps)
+					  #,stack #,sk #,fk))
+			   ((eq? 'unquote (car q^))
+			    (let ((p (syntax-car (syntax-cdr q))))
+			      #`(match-pats (#,p . #,ps)
+					    #,stack #,sk #,fk)))
+			   (else
+			    (let ((q-1 (syntax-car q)) (q-2 (syntax-cdr q)))
+			      #`(match-pats ((cons (quasiquote #,q-1) (quasiquote #,q-2)) . #,ps)
+					    #,stack #,sk #,fk))))))))
 
 )
